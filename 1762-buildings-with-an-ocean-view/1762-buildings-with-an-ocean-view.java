f93806d9aa9d597da13f1a811b264f519fa4d732
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2022-04-25 14:24:34 -0400

    Time: 4 ms (87.71%), Space: 87.4 MB (20.57%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.java
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.java
@@ -3,25 +3,23 @@ class Solution {
         
         //check for null input 
         if(heights == null || heights.length ==0)return heights;
+        int lastBuilding = Integer.MIN_VALUE;
         List<Integer> res = new ArrayList<>();
-        Stack<Integer> stack = new Stack<>();
         
-        for(int i=heights.length-1; i>=0; i--){
-            //if the building to the right is smaller than the curr then we can remove it from stack
-            while(!stack.isEmpty() && heights[stack.peek()] < heights[i]) stack.pop();
-            //if the stack is Empty then there is no building to the right to block the view
-            if(stack.isEmpty()) res.add(i);
-            
-            //add curr building to the stack
-            stack.push(i);
+        //going through buildings
+        for(int i=heights.length-1; i>=0 ; i--){
+            //if the current building is taller than the last one
+            if(heights[i]> lastBuilding){
+                res.add(i);
+                lastBuilding = heights[i];
+            }
         }
-        //prepare the result
         int[] result = new int[res.size()];
-        for(int i=res.size()-1; i>=0; i--) {
-            result[result.length-1-i]= res.get(i);
+        int index=0;
+        for(int i=res.size()-1; i>=0; i--){
+            result[index++] =res.get(i);
         }
         return result;
     }
 }
-
-//Time and space complexity : O(n)
+//Time and Space complexity of O(n)
