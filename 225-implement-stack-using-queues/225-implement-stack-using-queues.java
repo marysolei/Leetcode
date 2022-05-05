518d0d6ea4c17bce562b16ab8b10fa58cf7399e7
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2022-05-05 09:06:03 -0400

    Time: 0 ms (100.00%), Space: 42.1 MB (28.59%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.java
@@ -4,31 +4,31 @@ class MyStack {
     Queue<Integer> q2;
     int top;
     public MyStack() {
-        q1= new ArrayDeque<>();
-        q2= new ArrayDeque<>();
+        q1 = new ArrayDeque<>();
+        q2 = new ArrayDeque<>();
     }
-    
+    //Time complexity of O(n) and space complexity of O(1)
     public void push(int x) {
         q2.add(x);
-        top =x;
+        top = x;
         while(!q1.isEmpty()){
             q2.add(q1.remove());
         }
         Queue<Integer> tmp = q1;
-        q1= q2;
-        q2= tmp;
+        q1 = q2;
+        q2 = tmp;
     }
-    
+    //Time and Space complexity: O(1)
     public int pop() {
-       int res = q1.remove();
+        int res = q1.remove();
         if(!q1.isEmpty()) top = q1.peek();
         return res;
     }
-    
+    //Time and Space complexity: O(1)
     public int top() {
         return top;
     }
-    
+    //Time and Space complexity: O(1)
     public boolean empty() {
         return q1.isEmpty();
     }
