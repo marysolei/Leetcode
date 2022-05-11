d14de8e03c18451e72e1e878d1d8742075474815
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2022-05-11 09:18:44 -0400

    Time: 0 ms (100.00%), Space: 41.1 MB (35.01%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int countVowelStrings(int n) {
+   /* public int countVowelStrings(int n) {
      
         //check base cases
         if(n == 0)return 0;
@@ -8,8 +8,8 @@ class Solution {
         List<String> result = new ArrayList<>();
         helper(n, letters, curr, result,0);
         return result.size();
-    }
-    public void helper(int n, String letters, StringBuilder curr, List<String> result, int index){
+    } */
+  /*  public void helper(int n, String letters, StringBuilder curr, List<String> result, int index){
         if(n == curr.length()){
             result.add(curr.toString());
             return;
@@ -19,5 +19,18 @@ class Solution {
             helper(n,letters, curr, result, i);
             curr.deleteCharAt(curr.length()-1);
         }
-    }
+    }*/
+    
+     public int countVowelStrings(int n) {
+         int a =1, e=1, i=1, o=1, u=1;
+         while(n>1){
+             a = (a+e+i+o+u);
+             e = (e+i+o+u);
+             i = (i+o+u);
+             o = (o+u);
+             u = u;
+             n--;
+         }
+         return a+e+i+o+u;
+     }
 }
