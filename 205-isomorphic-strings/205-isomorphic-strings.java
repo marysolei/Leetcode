48425cc1912ac2f4172d55db48a22374c60acc42
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2022-07-12 19:44:53 -0400

    Time: 18 ms (33.19%), Space: 43.1 MB (50.67%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        
+        //check for null input and edge cases 
+        if(s == null || t== null)return false;
+        if(s.length() != t.length()) return false;
+        
+        HashMap<Character, Character> hmap = new HashMap<>();
+        for(int i=0; i< s.length(); i++){
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            if(hmap.containsKey(c1)){
+                if(hmap.get(c1) != c2) return false;
+            }else {
+                if(hmap.containsValue(c2)) return false;
+            }
+            hmap.put(c1,c2);
+        }
+        return true;
+    }
+}
