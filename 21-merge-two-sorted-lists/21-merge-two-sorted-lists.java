ec393cb08af7dadc321d569ef07c71c24b162be2
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2022-07-12 20:41:23 -0400

    Time: 1 ms (78.95%), Space: 43.4 MB (27.44%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -11,27 +11,26 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         
-        //check for edge cases
-        if(list1 == null)return list2;
-        if(list2 == null)return list1;
-        ListNode res = new ListNode();
+        //check for edge cases 
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+        ListNode res = new ListNode(-1);
         ListNode tmp = res;
-        while(list1 != null && list2 != null) {
-            if(list1.val < list2.val) {
+        
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val){
                 tmp.next = list1;
-                list1 = list1.next;
-            }else {
+                list1= list1.next;
+            } else {
                 tmp.next = list2;
                 list2 = list2.next;
             }
             tmp = tmp.next;
         }
-        if(list1 == null) {
-            tmp.next = list2;
-        }else {
-            tmp.next= list1;
-        }
+        if(list1 != null) tmp.next = list1;
+        else if(list2 != null)tmp.next = list2;
         return res.next;
     }
 }
-//Time and space complexity : O(n+m)
+
+//Time and Space complexity O(m+n) 
