cfd33120d26191159d8206eabfbf71a4e870915d
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2022-07-15 10:16:49 -0400

    Time: 3 ms (84.14%), Space: 46.8 MB (74.42%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -3,31 +3,35 @@ class Solution {
         
         //check for null input 
         if(grid == null || grid.length ==0)return 0;
-        //get rows and cols 
-        int rows = grid.length; 
+        //get rows and cols
+        int rows = grid.length;
         int cols = grid[0].length;
-        int area = 0;
-        int maxArea = 0;
-        for(int row = 0; row< rows; row++){
-            for(int col =0; col< cols; col++) {
-                if(grid[row][col] ==1){
-                    area = helper(grid, row, col,1);
+        int res = 0;
+        for(int r=0; r<rows; r++) {
+            for(int c=0;c<cols; c++){
+                //look for a cell which is part of an island
+                if(grid[r][c] ==1) {
+                    //get the island area
+                    int islandArea = helper(grid,r,c,1);
+                    res = Math.max(res, islandArea);
                 }
-                maxArea = Math.max(area, maxArea);
             }
         }
-        return maxArea;
+        return res;
     }
-    public int helper(int[][] grid, int row, int col, int area) {
-        //check for edge cases 
-        if(row<0 || row>= grid.length || col<0 || col>= grid[row].length || grid[row][col] ==0) return 0;
+    public int helper(int[][] grid, int row, int col, int curr){
+        //check for edge cases
+        if(row<0 || row>= grid.length || col<0 || col>= grid[0].length || grid[row][col] == 0)return 0;
         
-        //mark as visited
+        //mark the curr cell as visited
         grid[row][col] = 0;
-        area += helper(grid, row+1,col, area) +
-                helper(grid, row-1,col, area) +
-                helper(grid, row,col+1, area) +
-                helper(grid, row,col-1, area) ;
-        return area;
+        //check for 4-directionally neighbors
+        curr += helper(grid,row+1, col, curr)+
+                helper(grid,row-1, col, curr)+
+                helper(grid,row, col+1, curr)+
+                helper(grid,row, col-1, curr);
+        return curr;
     }
 }
+
+//Time complexity of O(r*c)
