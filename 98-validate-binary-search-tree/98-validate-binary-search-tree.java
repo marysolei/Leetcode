a149d1b7518af892df72b10ccf39b2975e8dfed3
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2022-07-18 11:11:43 -0400

    Time: 1 ms (61.12%), Space: 44.4 MB (23.10%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -16,20 +16,20 @@
 class Solution {
     public boolean isValidBST(TreeNode root) {
         
-        //check for null input 
+        //check for null input
         if(root == null)return true;
         //base case
         if(root.left == null && root.right == null)return true;
         return helper(root, null, null);
     }
-    public boolean helper(TreeNode root, Integer min, Integer max){
+    public boolean helper(TreeNode root, Integer min , Integer max){
+        
         if(root == null)return true;
-        //base case
-        if((min != null &&root.val<= min) || (max != null && root.val >= max))return false;
-        //call func for children
+        //base case 
+        if((min != null && root.val <= min) || (max != null &&root.val >= max))return false;
+        //call for children
         return helper(root.left, min, root.val) && helper(root.right, root.val, max);
     }
 }
-
 //Time complexity :O(N) since we visit each node exactly once.
 //Space complexity : O(N) since we keep up to the entire tree.
