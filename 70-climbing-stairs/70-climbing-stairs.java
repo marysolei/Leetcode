f0b56897b634cdc0f595d622142dac54f2337a1c
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2022-07-20 11:34:30 -0400

    Time: 0 ms (100.00%), Space: 40.8 MB (58.04%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,17 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-        
-        //check for base cases
-        if(n == 0 || n == 1)return 1;
-        int[] dp = new int[n+1];
-        dp[0] = 1;
-        dp[1] = 1;
-        for(int i=2; i<=n ; i++) {
-            dp[i] = dp[i-1]+dp[i-2];
+       
+        if(n == 0)return 0;
+        int[] memo = new int[n+1];
+        memo[0]= 1;
+        memo[1]=1;
+        for(int i=2; i<=n ; i++){
+            memo[i] = memo[i-1] + memo[i-2];
         }
-        return dp[n];
+        return memo[n];
     }
 }
-
-/*Time complexity : O(n) Single loop upto n.
-Space complexity : O(n). dp array of size n is used.*/
