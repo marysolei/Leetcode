622189e49922be912e71983f9f2d3a94ef53fe6d
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2022-07-21 17:09:15 -0400

    Time: 1 ms (53.06%), Space: 41 MB (40.20%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        
+        //check for null input 
+        if(m == 0 || n == 0)return 0;
+        int[][] memo = new int[m][n];
+        memo[0][0]= 1;
+        for(int r=1; r< m; r++)memo[r][0]= 1;
+        for(int c=1; c< n ;c++)memo[0][c]= 1;
+        for(int r=1; r< m; r++){
+            for(int c=1; c< n; c++){
+                memo[r][c] = memo[r-1][c] +memo[r][c-1];
+            }
+        }
+        return memo[m-1][n-1];
+    }
+}
