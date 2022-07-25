f0a520470fc74a29eb6841421fce0828a846325f
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2022-07-25 10:32:38 -0400

    Time: 3 ms (41.78%), Space: 42 MB (17.95%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,27 +1,21 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
         
-        
-        //check edge cases
-        if (stones == null || stones.length == 0)return 0 ; 
+        //check for null input 
+        if(stones == null || stones.length ==0)return 0;
         //use a priority queue to dynamically sort the stones
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b) ->b-a);
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>((a,b) -> b-a);
         //fill in the queue
-        for (int stone : stones) {
-            maxHeap.add(stone);
-        }
+        for(int stone: stones) maxHeap.add(stone);
         //get the sorted stones from the queue and follow the stash rules
-        while(maxHeap.size()>1) {
-            int y= maxHeap.poll();
+        while(maxHeap.size()>1){
+            int y = maxHeap.poll();
             int x = maxHeap.poll();
-            if (x!=y) {
-                maxHeap.add(y-x);
-            }
+            if(x!=y) maxHeap.add(y-x);
         }
-        //return the last stone in the maxHeap
-        return maxHeap.isEmpty()? 0: maxHeap.poll();
+        //return the last stone in the maxHeap or 0 otherwise
+        return maxHeap.isEmpty()? 0 : maxHeap.poll();
     }
 }
-
 //Time Complexity: O(NlogN) sorting
 //Space Complexity: O(N) because of maxHeap
