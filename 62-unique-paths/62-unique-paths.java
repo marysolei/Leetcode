b86876ec1404f07fa1416bbdb0297735aea47035
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2022-08-01 18:14:54 -0400

    Time: 0 ms (100.00%), Space: 41.3 MB (27.25%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,17 +1,18 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         
-        //check for null input 
-        if(m == 0 || n == 0)return 0;
-        int[][] memo = new int[m][n];
-        memo[0][0]= 1;
-        for(int r=1; r< m; r++)memo[r][0]= 1;
-        for(int c=1; c< n ;c++)memo[0][c]= 1;
+        //check for null input
+        if(m *n ==0)return 0;
+        int[][] grid = new int[m][n];
+        grid[0][0] = 1;
+        for(int r=1; r<m; r++) grid[r][0] = 1;
+        for(int c=1; c<n ; c++)grid[0][c] = 1;
         for(int r=1; r< m; r++){
-            for(int c=1; c< n; c++){
-                memo[r][c] = memo[r-1][c] +memo[r][c-1];
+            for(int c=1; c<n; c++){
+                grid[r][c] = grid[r-1][c] + grid[r][c-1];
             }
         }
-        return memo[m-1][n-1];
+        return grid[m-1][n-1];
     }
 }
+//Time and space complexity of O(m*n)
