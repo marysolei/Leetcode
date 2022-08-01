479cd1b87aa5e5dc3f9aa3ca1c58b42802520be2
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2022-08-01 18:20:23 -0400

    Time: 0 ms (100.00%), Space: 41.7 MB (50.06%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.java
+++ 226-invert-binary-tree/226-invert-binary-tree.java
@@ -17,8 +17,8 @@ class Solution {
     public TreeNode invertTree(TreeNode root) {
         
         //base case 
-        if(root == null)return root;
-        //call func for children 
+        if(root == null) return root;
+        //call func for children
         TreeNode left = invertTree(root.left);
         TreeNode right = invertTree(root.right);
         root.left = right;
@@ -26,5 +26,6 @@ class Solution {
         return root;
     }
 }
+
 //Time Complexity O(n) since we visit each node once
 //Space Complexity O(h) where h is the height of tree for call stacks, its worstcase is O(n)
