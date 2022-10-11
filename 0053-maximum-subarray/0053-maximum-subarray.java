9cf8e93385469c8fd29496e2887349a50939b379
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2022-10-11 09:24:03 -0400

    Time: 1 ms (100.00%), Space: 73.2 MB (72.93%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        if(nums == null || nums.length ==0)return 0;
+        int currSum = nums[0];
+        int maxSum = nums[0];
+        for(int i=1;i< nums.length; i++){
+            if(currSum <0) currSum =0;
+            currSum += nums[i];
+            maxSum = Math.max(maxSum, currSum);
+        }
+        return maxSum;
+    }
+}
