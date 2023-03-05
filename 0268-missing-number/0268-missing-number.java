f1a30f2bbd8c0211632c8803dcad22b6469f01d1
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2023-03-05 16:44:11 -0500

    Time: 0 ms (100.00%), Space: 43.4 MB (31.56%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,13 +1,10 @@
 class Solution {
     public int missingNumber(int[] nums) {
         
-        if(nums == null || nums.length ==0)return 0;
         int currSum = 0;
-        int totalSum = 0;
-        for(int num : nums) currSum += num;
-        for (int i=0; i<= nums.length; i++) totalSum += i;
-        return totalSum - currSum;
+        int totalSum= 0;
+        for(int i=0; i<=nums.length ; i++) totalSum+= i;
+        for(int num: nums) currSum += num;
+        return totalSum-currSum;
     }
 }
-//Time complexity: O(n)
-//space complexity: O(1)
