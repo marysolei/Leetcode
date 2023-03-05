5d5ebb6303d70e10141cca2b41aeec3c1f1250a4
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2023-03-05 17:27:42 -0500

    Time: 0 ms (100.00%), Space: 42.2 MB (38.25%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -11,16 +11,14 @@
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         
-        if(head == null) return head;
-        ListNode res = new ListNode(-1);
-        res.next = head;
-        ListNode curr = res.next;
+        ListNode curr = head;
         while(curr != null && curr.next != null){
-            if (curr.val == curr.next.val){
-                curr.next = curr.next.next;
-            }
+            if(curr.val == curr.next.val) curr.next = curr.next.next;
             else curr = curr.next;
         }
-        return res.next;
+        return head;
     }
 }
+
+//Time Complexity = O(n)
+//Space Complexity = O(1)
