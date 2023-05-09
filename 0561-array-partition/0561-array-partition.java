19d8418135f9aec5c019b7def98fdf80a059a0d6
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2023-05-09 18:09:21 -0400

    Time: 14 ms (28.41%), Space: 44.9 MB (44.54%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        
+        if(nums == null || nums.length ==0)return 0;
+        Arrays.sort(nums);
+        int res = 0;
+        for(int i=0; i<nums.length; i+=2){
+            res += nums[i];
+        }
+        return res;
+    }
+}
