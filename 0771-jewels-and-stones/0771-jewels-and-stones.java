20daef60c203e96828dec0bed7e9382cf80b16bb
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2023-05-09 19:38:44 -0400

    Time: 1 ms (91.39%), Space: 41.2 MB (13.76%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,12 +1,12 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         
-        if(stones == null || stones.length() ==0) return 0;
-        HashSet<Character> jewelSet = new HashSet<>();
-        int cnt = 0;
-        for(char j: jewels.toCharArray()) jewelSet.add(j);
-        for (char stone: stones.toCharArray()){
-            if(jewelSet.contains(stone)) cnt++;
+        if(jewels == null || jewels.length() ==0)return 0;
+        HashSet<Character> jSet = new HashSet<>();
+        int cnt=0;
+        for(char j : jewels.toCharArray()) jSet.add(j);
+        for(char s : stones.toCharArray()){
+            if(jSet.contains(s)) cnt++;
         }
         return cnt;
     }
