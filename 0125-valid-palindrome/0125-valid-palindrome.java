6c4822132fa89a8b2b31d65778d20e367d06ebb8
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2023-05-09 20:01:56 -0400

    Time: 27 ms (35.64%), Space: 43.5 MB (32.03%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean isPalindrome(String s) {
-     
+        
         if(s == null || s.length() ==0)return false;
         s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
-        s = s.replaceAll(" ", "");
         int left = 0;
         int right = s.length()-1;
-        
-        while(left <= right){
-            if(s.charAt(left) != s.charAt(right)) return false;
+        while(left<right){
+            if(s.charAt(left) != s.charAt(right))return false;
             left++;
             right--;
         }
