a29ec555aa9819411e930ce8709aa6211d3cb63d
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2023-08-23 19:06:09 -0400

    Time: 1 ms (88.21%), Space: 39.3 MB (77.51%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean isHappy(int n) {
         
-       HashSet<Integer> visited = new HashSet<>();
-       while(n!=1 && !visited.contains(n)) {
-           visited.add(n);
-           n = getSum(n);
-       }
+      HashSet<Integer> visited = new HashSet<>();
+        while(n != 1 && !visited.contains(n)){
+            visited.add(n);
+            n = getSum(n);
+        }
         return n==1;
     }
     public int getSum(int n){
-        int sum =0;
-        while(n>0){
-            int digit = n%10;
-            sum+= digit*digit;
+        int sum = 0;
+        while(n >0) {
+            int digit= n%10;
+            sum += digit*digit;
             n/=10;
         }
         return sum;
