c203ee4caa0f1c6398e0db0e7022d40d1d165037
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2023-09-07 15:56:57 -0400

    Time: 1 ms, Memory: 41 MB - LeetHub

--- jewels-and-stones/jewels-and-stones.java
+++ jewels-and-stones/jewels-and-stones.java
@@ -2,17 +2,14 @@ class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         
         //check for null input 
-        if(jewels == null || stones == null)return 0;
-        HashSet<Character> jewelSet = new HashSet();
-        for(char jewel : jewels.toCharArray()){
-            jewelSet.add(jewel);
-        }
-        int res = 0;
-        for(char stone : stones.toCharArray()){
+        if(jewels == null || stones == null) return 0;
+        HashSet<Character> jewelSet = new HashSet<>();
+        int res =0;
+        for (char j : jewels.toCharArray()) jewelSet.add(j);
+        for(char stone: stones.toCharArray()){
             if(jewelSet.contains(stone)) res++;
         }
         return res;
     }
 }
-//Time complexity: O(max(s,j))
-//space complexity: O(j)
+//Time and Space complexity: O(n)
