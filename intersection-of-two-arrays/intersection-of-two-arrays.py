f9e1867bc3b490b1dbb67cf19201914b625dea1e
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2023-09-26 19:47:36 -0400

    Time: 68 ms, Memory: 16.5 MB - LeetHub

---
+++ intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -0,0 +1,7 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = []
+        for num in nums1:
+            if(num not in res and num in nums2):
+                res.append(num)
+        return res
