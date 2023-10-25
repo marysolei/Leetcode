aba78adf49ebac13d3dd18bdb1103201848c41df
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2023-10-24 21:14:53 -0400

    Time: 2 ms, Memory: 44.3 MB - LeetHub

--- inorder-successor-in-bst/inorder-successor-in-bst.java
+++ inorder-successor-in-bst/inorder-successor-in-bst.java
@@ -7,6 +7,33 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+/*Algorithm:
+
+We start our traversal with the root node and continue the traversal until our current node reaches a null value i.e. there are no more nodes left to process.
+At each step we compare the value of node p with that of node.
+If p.val >= node.val that implies we can safely discard the left subtree since all the nodes there including the current node have values less than p.
+However, if p.val < node.val, that implies that the successor must lie in the left subtree and that the current node is a potential candidate for inorder successor. Thus, we update our local variable for keeping track of the successor, successor, to node.
+Return successor
+*/
+class Solution {
+    
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        
+        TreeNode successor = null;
+        while (root != null){
+            if(root.val <= p.val) {
+                root = root.right;
+            }else {
+                successor = root;
+                root = root.left;
+            }
+        }
+        return successor;
+    }
+}
+
+/*simple solution
 class Solution {
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
         if (root == null)return null;
@@ -27,4 +54,4 @@ class Solution {
         result.add(root);
         helper(root.right, result);
     }
-}
+}*/
