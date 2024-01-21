9e3bd3c6d23967d4e29dd94afca7ab7c90b5e058
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2024-01-21 10:30:43 -0500

    Time: 118 ms, Memory: 17.9 MB - LeetHub

--- find-pivot-index/find-pivot-index.py
+++ find-pivot-index/find-pivot-index.py
@@ -1,11 +1,11 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        if nums is None: return -1
-        totalSum =sum(nums);
-        currSum =0
-        for i,num in enumerate(nums):
-            if currSum == totalSum-currSum-num: return i
-            currSum += nums[i]
+        if nums is None: return
+        totalSum = sum(nums)
+        currSum = 0
+        for index, num in enumerate(nums):
+            if totalSum - currSum - num == currSum: return index
+            currSum += num
         return -1
-# time compelxity:O(n)
-#space complexity:O(1)
+    #time complexity:O(n)
+    #space complexity:O(1)
