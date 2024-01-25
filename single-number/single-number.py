d5c5b2f3f4df659c23e54bf394826be8ce5cc8aa
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2024-01-24 20:23:01 -0500

    Time: 109 ms, Memory: 19 MB - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -1,6 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        res = 0;
+        res = 0
         for num in nums:
-            res = res^num
-        return res;
+            res ^= num
+        return res
+
+    #time complexity O(n)
+    #space complexity O(1)
