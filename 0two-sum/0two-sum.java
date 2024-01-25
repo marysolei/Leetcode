71c96505eb6de96c9978ae67059b6ed56b45fd91
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2024-01-25 08:46:52 -0500

    Time: 2 ms, Memory: 45.2 MB - LeetHub

--- 0two-sum/0two-sum.java
+++ 0two-sum/0two-sum.java
@@ -1,20 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-     
-        //check for null input
-        if(nums == null)return nums;
-        HashMap<Integer, Integer> hmap = new HashMap<>();
-        for(int i= 0; i< nums.length; i++){
-            hmap.put(target-nums[i],i);
-        }
-        int[] res = new int[2];
-        for(int i=0;i< nums.length; i++){
-            if(hmap.containsKey(nums[i]) && i != hmap.get(nums[i])){
-               res[0]= i;
-                res[1] = hmap.get(nums[i]);
+        //check for null input 
+        if (nums == null || nums.length ==0)return nums;
+        int[] res = new int[]{-1,-1};
+        HashMap<Integer, Integer> mapping = new HashMap<>();
+        for(int i =0; i<nums.length; i++){
+            if(mapping.containsKey(nums[i]) && mapping.get(nums[i])!=i){
+                res[0]= i;
+                res[1]= mapping.get(nums[i]);
+                return res;
             }
+            mapping.put(target-nums[i],i);     
         }
         return res;
     }
 }
+
 //time and space complexity: O(n)
