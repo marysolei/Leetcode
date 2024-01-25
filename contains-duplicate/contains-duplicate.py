e9d0b530c509e7b9e3d5f1cafe17e4089fb6b577
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2024-01-24 20:17:03 -0500

    Time: 424 ms, Memory: 32 MB - LeetHub

--- contains-duplicate/contains-duplicate.py
+++ contains-duplicate/contains-duplicate.py
@@ -1,14 +1,10 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        #check for null input 
-        if(not nums) :
-            return False
-        hset = set()
+        #check for null input
+        if nums is None: return False
+        numSet = set()
         for num in nums:
-            if num in hset:
-                return True
-            hset.add(num)
-            
+            if num in numSet: return True
+            numSet.add(num)
         return False
-                
-    
+    #time and space complexity:O(n)
