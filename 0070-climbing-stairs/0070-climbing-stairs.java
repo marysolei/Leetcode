70c8c1f90473e0077dd766daa1137e247cbc6734
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2024-01-26 19:53:01 -0500

    Time: 0 ms (100.00%), Space: 40.4 MB (27.38%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-        
-        if(n == 0)return 1;
+        if (n == 0 || n==1) return 1;
         int[] memo = new int[n+1];
-        memo[0] = 1;
-        memo[1] = 1;
-        for(int i=2; i<= n; i++){
-            memo[i] = memo[i-1] + memo[i-2];
+        memo[0]= 1;
+        memo[1]= 1;
+        for(int i=2; i<=n; i++){
+            memo[i] = memo[i-1]+memo[i-2];
         }
         return memo[n];
     }
 }
-
-//Time and space complexity: O(n)
+//time and space complexity: O(n)
