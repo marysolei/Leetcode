3cb7ff85cc8c36d13d01cd0d680e959a28a2af29
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2024-01-29 20:46:17 -0500

    Time: 16 ms (35.30%), Space: 48.8 MB (18.37%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,18 +1,21 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        
-        if(nums == null || nums.length==0) return nums;
-        HashMap<Integer,Integer> freqMap = new HashMap<>();
-        for(int num: nums) freqMap.put(num, freqMap.getOrDefault(num,0)+1);
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a,b)-> freqMap.get(a)- freqMap.get(b));
+        //check for null input 
+        if(nums == null || nums.length ==0)return nums;
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+        for(int num: nums)freqMap.put(num, freqMap.getOrDefault(num,0)+1);
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a,b) -> freqMap.get(a) - freqMap.get(b));
         for(int key: freqMap.keySet()){
             minHeap.add(key);
             if(minHeap.size()>k) minHeap.poll();
         }
-        int[] res = new int[k];
-        for(int i=0;i<k; i++) res[i] = minHeap.poll();
+        int[]res = new int[k];
+        int index=0;
+        while(!minHeap.isEmpty()){
+            res[index++] = minHeap.poll();
+        }
         return res;
     }
 }
-//Time complexity : O(Nlogk) if k<N and O(N) in the particular case of N=k. That ensures time complexity to be better than O(NlogN).
-// Space complexity : O(N+k) to store the hash map with not more N elements and a heap with k elements. 
+//time complexity: O(Nlogk)
+//space complexity:O(N+k)
