cec1360e4d586a92dbdb821bd68602a6edce3b78
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2024-02-05 18:40:31 -0500

    Time: 144 ms (62.77%), Space: 41.7 MB (32.48%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,32 +1,28 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        
-        //check for null input 
-        if(board == null || board.length ==0)return false;
-        int rows = board.length;
+        //check for null input
+        if(board == null || board.length ==0 || word.length()==0) return false;
+        int rows= board.length;
         int cols = board[0].length;
         for(int r=0; r< rows; r++){
             for(int c=0; c< cols;c++){
-                if(board[r][c] == word.charAt(0)){
-                    if(helper(board, word, r, c, 0)) return true;
-                }
+               if(word.charAt(0) == board[r][c]){
+                   if(helper(board, word, r,c,0)) return true;
+               }
             }
         }
-       return false;
+        return false;
     }
-    public boolean helper(char[][] board, String word, int row , int col, int index){
-        
+    boolean helper(char[][] board, String word, int r, int c, int index){
         if(index == word.length()) return true;
-        //check edge cases
-        if(row<0 || row>= board.length || col<0 || col>= board[0].length || word.charAt(index) != board[row][col]) return false;
-        
-        char tmp = board[row][col];
-        board[row][col] = '-';
-        boolean isFound = helper(board, word, row+1, col, index+1)||
-                          helper(board, word, row-1, col, index+1)||
-                          helper(board, word, row, col+1, index+1)||
-                          helper(board, word, row, col-1, index+1);
-        board[row][col] =tmp;
+        if(r<0 || r>= board.length || c<0 || c>= board[0].length || board[r][c] != word.charAt(index)) return false;
+        char tmp = board[r][c];
+        board[r][c] = '-';
+        boolean isFound = helper(board, word, r+1, c, index+1)||
+                          helper(board, word, r-1, c, index+1)||
+                          helper(board, word, r, c+1, index+1)||
+                          helper(board, word, r, c-1, index+1);
+        board[r][c] = tmp;
         return isFound;
     }
 }
