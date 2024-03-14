4577cb0c95aa733c728fbcf2c72c57b090d3a7ee
Mary Soleimani <48185634+marysolei@users.noreply.github.com>
2024-03-14 08:42:06 -0400

    Time: 56 ms (78.14%), Space: 18 MB (18.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,18 +1,12 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        //check for null input
-        if(nums == null || nums.length ==0)return nums;
-        int[] res = new int[]{-1,-1};
-        HashMap<Integer, Integer> hmap = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            if(hmap.containsKey(nums[i]) && hmap.get(nums[i])!= i){
-                res[0]= i;
-                res[1] = hmap.get(nums[i]);
-                return res;
-            }
-            hmap.put(target-nums[i],i);
-        }
-        return res;
-    }
-}
-//time and space complexity: O(n)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        if nums is None: return
+        hmap = {}
+        for i in range(len(nums)):
+            if nums[i] in hmap and hmap[nums[i]]!= i:
+                return [hmap[nums[i]],i]
+            else:
+                hmap[target-nums[i]]=i
+        return[]
+    
+#time and space complexity: O(n)
